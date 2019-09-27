b1cf88edd3b0d6b3f7656057719397d4f89666eb
ZXZhuang <2993968012@qq.com>
2019-09-27 11:15:45 +0800

    advanceSearch

---
+++ src/components/inputFloat.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="2" :offset="0">
+        <div style="margin-top: 10px">{{ name }}</div>
+      </el-col>
+      <el-col :span="3" :offset="0">
+        <el-input v-model="content1" style="width: 100%"></el-input>
+      </el-col>
+      <el-col :span="1" :offset="0">
+        <div style="margin-top: 10px">——</div>
+      </el-col>
+      <el-col :span="3" :offset="0">
+        <el-input v-model="content2" style="width: 100%;"></el-input>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      name:{
+        type: String,
+        required: true
+      },
+      index:{
+        type: Number,
+        required: true
+      }
+    },
+    data(){
+      return{
+        content1: '',
+        content2: '',
+        keyword: {
+          name: this.name,
+          value1: '',
+          value2: '',
+        },
+      }
+    },
+    methods:{
+
+    },
+    watch:{
+      content1:{
+        handler(val,oldval){
+          this.keyword.value1 = val
+          let temp = {keyword: this.keyword, index: this.index}
+          this.$emit('uploaddata',temp)
+        }
+      },
+      content2:{
+        handler(val,oldval){
+          this.keyword.value2 = val
+          let temp = {keyword: this.keyword, index: this.index}
+          this.$emit('uploaddata',temp)
+        },
+        immediate: true
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/inputInt.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="2" :offset="0">
+        <div style="margin-top: 10px">{{ name }}</div>
+      </el-col>
+      <el-col :span="7" :offset="0">
+        <el-input v-model="content" style="width:100%"></el-input>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+    export default {
+      props:{
+        name:{
+          type: String,
+          required: true
+        },
+        index:{
+          type: Number,
+          required: true
+        }
+      },
+      data(){
+        return{
+          content: "",
+          keyword:{
+            name: this.name,
+            value: '',
+          }
+        }
+      },
+      methods:{
+
+      },
+      watch:{
+        content:{
+          handler(val,oldval){
+            this.keyword.value = val;
+            let temp = {keyword: this.keyword, index: this.index}
+            this.$emit('uploaddata',temp)
+          },
+          immediate: true
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
